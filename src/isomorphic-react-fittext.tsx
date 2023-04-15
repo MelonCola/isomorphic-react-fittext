86e61fde3e00e53f5dd729b348a7ba52d1593cb0
FrostTaco <36684959+MelonCola@users.noreply.github.com>
2023-04-15 01:02:59 -0400

    sure

--- src/isomorphic-react-fittext.tsx
+++ src/isomorphic-react-fittext.tsx
@@ -5,70 +5,65 @@ const nodes = new Map<HTMLElement, ReactFitTextProps>();
 let updateQueued = false;
 
 function updateFontSize() {
-  updateQueued = true;
-  Array.from(nodes, ([element, options]) => ({
-    element,
-    options
-  })).forEach(x => {
-    const minimumDimension = Math.min(
-      x.element.offsetWidth,
-      x.element.offsetHeight
-    );
-    x.element.style.fontSize = `${Math.min(
-      Math.max(
-        minimumDimension / (x.options.compressor * 10),
-        x.options.minFontSize
-      ),
-      x.options.maxFontSize
-    )}px`;
-  });
+	updateQueued = true;
+	Array.from(nodes, ([element, options]) => ({
+		element,
+		options,
+	})).forEach((x) => {
+		const minimumDimension = Math.min(x.element.offsetWidth, x.element.offsetHeight);
+		x.element.style.fontSize = `${Math.min(
+			Math.max(minimumDimension / (x.options.compressor * 10), x.options.minFontSize),
+			x.options.maxFontSize
+		)}px`;
+	});
 }
 
 interface ReactFitTextProps {
-  children: ReactNode;
-  compressor: number;
-  minFontSize: number;
-  maxFontSize: number;
+	children: ReactNode;
+	compressor: number;
+	minFontSize: number;
+	maxFontSize: number;
 }
 
 const ReactFitText = (props: ReactFitTextProps) => {
-  let childRef: HTMLElement | undefined = undefined;
+	let childRef: HTMLElement | undefined = undefined;
 
-  useEffect(() => {
-    window.addEventListener("resize", updateFontSize);
-    window.addEventListener("load", updateFontSize);
-    return () => {
-      if (childRef) {
-        nodes.delete(childRef);
-      }
-      window.removeEventListener("resize", updateFontSize);
-      window.removeEventListener("load", updateFontSize);
-    };
-  }, [window]);
+	useEffect(() => {
+		if (!window) return;
+		window.addEventListener("resize", updateFontSize);
+		window.addEventListener("load", updateFontSize);
+		return () => {
+			if (childRef) {
+				nodes.delete(childRef);
+			}
+			window.removeEventListener("resize", updateFontSize);
+			window.removeEventListener("load", updateFontSize);
+		};
+	}, [window]);
 
-  useEffect(() => {
-    if (!updateQueued && typeof window !== "undefined") {
-      window.requestAnimationFrame(updateFontSize);
-    }
-  }, [updateQueued, window]);
+	useEffect(() => {
+		if (!updateQueued && typeof window !== "undefined") {
+			window.requestAnimationFrame(updateFontSize);
+		}
+	}, [updateQueued, window]);
 
-  updateFontSize();
+	updateFontSize();
 
-  return Children.map(props.children, (child: any) =>
-    cloneElement(child, {
-      ref: (element: HTMLElement) => {
-        if (element) {
-          nodes.set(element, props);
-        }
-        childRef = element;
-      }
-    })
-  )[0];
+	return Children.map(props.children, (child: any) =>
+		cloneElement(child, {
+			ref: (element: HTMLElement) => {
+				if (element) {
+					nodes.set(element, props);
+				}
+				childRef = element;
+			},
+		})
+	)[0];
 };
 
 ReactFitText.defaultProps = {
-  compressor: 1.0,
-  minFontSize: Number.NEGATIVE_INFINITY,
-  maxFontSize: Number.POSITIVE_INFINITY
+	compressor: 1.0,
+	minFontSize: Number.NEGATIVE_INFINITY,
+	maxFontSize: Number.POSITIVE_INFINITY,
 };
 export default ReactFitText;
